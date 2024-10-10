1e3ba1f53d40ce95954a701299219d214d2f675c
Michael Velez <michael.tene@outlook.es>
2024-10-10 19:19:56 +0200

    Agregar archivos de proyecto.

---
+++ Controllers/EmpleadoController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AppCRUD.Data;
+using AppCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCRUD.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        private readonly AppDBContext _appDbContext;
+        public EmpleadoController(AppDBContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            List<Empleado> lista = await _appDbContext.Empledos.ToListAsync();
+            return View(lista);
+        }
+        [HttpGet]
+        public IActionResult Nuevo()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Nuevo(Empleado empleado)
+        {
+            await _appDbContext.Empledos.AddAsync(empleado);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Editar(int id)
+        {
+            Empleado empleado = await _appDbContext.Empledos.FirstAsync(e => e.IdEmpleado == id);
+            return View(empleado);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Editar(Empleado empleado)
+        {
+            _appDbContext.Empledos.Update(empleado);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            Empleado empleado = await _appDbContext.Empledos.FirstAsync(e => e.IdEmpleado == id);
+            _appDbContext.Empledos.Remove(empleado);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
+        }
+
+    }
+}
